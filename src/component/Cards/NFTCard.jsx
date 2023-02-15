abde271aacbbc49220040d6cab47043de2878cae
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-15 16:22:30 +0530

    Style: Ui fixes

--- src/component/Cards/NFTCard.jsx
+++ src/component/Cards/NFTCard.jsx
@@ -1,8 +1,8 @@
 function NFTCard({ content, isAvailable }) {
   const availabilty = isAvailable ? "Available" : "Not Available";
   return (
-    <div className="bg-[#3b3b3b] md:sticky md:top-0 md:left-0 rounded-2xl max-h-[365px]  lg:max-h-[57vh] lg:max-w-[40vw]">
-      <div className="h-[60vw] md:h-[30vh] lg:h-[50vh] lg:w-[40vw]">
+    <div className="bg-[#3b3b3b] md:sticky md:top-0 md:left-0 rounded-2xl max-h-[365px] md:max-w-[49vw] md:max-h-[40vw] lg:max-h-[38vw] lg:max-w-[37vw]">
+      <div className="h-[70vw] md:h-[35vw] lg:h-[35vw] lg:w-[37vw]">
         <img
           className="w-full h-full bg-cover bg-center rounded-2xl"
           src={content}
