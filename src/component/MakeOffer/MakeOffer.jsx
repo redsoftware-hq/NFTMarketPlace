49a4cc75b0ee3876256e31f008b6ba3462a4aa85
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-15 16:05:36 +0530

    Add: Component for buting

---
+++ src/component/MakeOffer/MakeOffer.jsx
@@ -0,0 +1,25 @@
+import PrimaryButton from "../common/PrimaryButton";
+
+function MakeOffer({ nft }) {
+  return (
+    <div className="p-2 flex flex-col gap-3 w-full">
+      <div className="bg-[#3B3B3B] p-3 flex gap-2 justify-evenly rounded-2xl max-w-[135px]">
+        {nft.artistAvatar && <img src={nft.artistAvatar} />}
+        <span className="text-white font-space-mono font-medium text-base">
+          {nft.artistName}
+        </span>
+      </div>
+      <h2 className="text-white text-2xl font-work-sans font-semibold">
+        {nft.name}
+      </h2>
+      <div className="text-[#858584] font-space-mono text-sm flex flex-wrap gap-2 items-center justify-between max-w-[415px] w-full">
+        <h1 className="text-white text-3xl">{nft.price} ETH</h1>
+        <h2>${nft.fiatPrice} USD</h2>
+        <span className="font-work-sans">Include fees</span>
+      </div>
+      <PrimaryButton btnName="Buy Now" />
+    </div>
+  );
+}
+
+export default MakeOffer;
