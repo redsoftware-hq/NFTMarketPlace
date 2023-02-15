271a31c83d5c5c56baf17265742f077cdbcc9136
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-15 18:09:23 +0530

    feat: Partial Implementation of properties component

---
+++ src/component/Properties/Properties.jsx
@@ -0,0 +1,17 @@
+import Accordion from "../common/Accordion";
+
+function Properties() {
+  return (
+    <div>
+      <Accordion />
+      <p className="text-[#858584] font-space-mono font-light text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
+}
+
+export default Properties;
