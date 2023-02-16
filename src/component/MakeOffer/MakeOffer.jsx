ca4c81665a4a4bde24ba26a55072eb35f24f5713
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-16 11:42:36 +0530

    Style: Ui fixes

--- src/component/MakeOffer/MakeOffer.jsx
+++ src/component/MakeOffer/MakeOffer.jsx
@@ -12,8 +12,8 @@ function MakeOffer({ nft }) {
       <h2 className="text-white text-2xl font-work-sans font-semibold">
         {nft.name}
       </h2>
-      <div className="text-[#858584] font-space-mono text-sm flex flex-wrap gap-2 items-center justify-between max-w-[415px] w-full">
-        <h1 className="text-white text-3xl">{nft.price} ETH</h1>
+      <div className="text-[#858584] font-space-mono text-sm flex flex-wrap gap-2 items-center justify-between w-full">
+        <h1 className="text-white text-3xl basis-3/4">{nft.price} ETH</h1>
         <h2>${nft.fiatPrice} USD</h2>
         <span className="font-work-sans">Include fees</span>
       </div>
