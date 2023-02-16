95a67ec496713eb64db3aceda23a1b988b9425a0
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-16 16:07:47 +0530

    Remove: removed repeated content

--- src/component/Properties/Properties.jsx
+++ src/component/Properties/Properties.jsx
@@ -4,12 +4,6 @@ function Properties() {
   return (
     <div>
       <Accordion />
-      <p className="text-[#858584] font-space-mono font-light text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
     </div>
   );
 }
