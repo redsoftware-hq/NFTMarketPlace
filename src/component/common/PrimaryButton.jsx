626f79074cde5a1d31c245ece5c5a75e5ad985ad
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-16 16:05:43 +0530

    Style: accent color updated

--- src/component/common/PrimaryButton.jsx
+++ src/component/common/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 export default function PrimaryButton({ btnName }) {
   return (
-    <button className="py-3 px-10 bg-[#A259FF] rounded-2xl capitalize text-white text-base md:text-lg gap-2">
+    <button className="py-3 px-10 bg-[#F15623] rounded-2xl capitalize text-white text-base md:text-lg gap-2">
       {btnName}
     </button>
   );
