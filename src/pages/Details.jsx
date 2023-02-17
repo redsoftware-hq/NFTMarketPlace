c2d8512735fa826822bf08a089b9e3b501a44424
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-17 12:24:27 +0530

    Remove: unused import

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,6 @@
 import AvailableListings from "../component/AvailableListings/AvailableListings";
 import NFTCard from "../component/Cards/NFTCard";
 import MakeOffer from "../component/MakeOffer/MakeOffer";
-import Navbar from "../component/Navbar/Navbar";
 import Properties from "../component/Properties/Properties";
 import artistAvatar from "../assets/details/a1.png";
 import nftContent from "../assets/details/bg.png";
@@ -17,15 +16,14 @@ const nft = {
 function Details() {
   return (
     <>
-      <Navbar />
-      <div className="flex flex-col p-2 gap-4 md:flex-row lg:justify-between m-auto max-w-[1280px]">
+      <section className="flex flex-col p-2 gap-4 md:flex-row lg:justify-between m-auto max-w-[1280px]">
         <NFTCard content={nft.content} isAvailable={nft.isAvailable} />
         <div className="md:w-3/6">
           <MakeOffer nft={nft} />
           <AvailableListings />
           <Properties />
         </div>
-      </div>
+      </section>
     </>
   );
 }
