e84e1aca4c2780cce996e1ac96f769fc8e68fa20
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-17 16:57:56 +0530

    fix: updated as per common data

--- src/component/Details/index.jsx
+++ src/component/Details/index.jsx
@@ -1,25 +1,16 @@
 import NFTCard from "./Cards/NFTCard";
-import nftContent from "../../assets/details/bg.png";
-import artistAvatar from "../../assets/details/a1.png";
 import MakeOffer from "./MakeOffer/MakeOffer";
 import Properties from "./Properties/Properties";
 import AvailableListings from "./AvailableListings/AvailableListings";
-
-const nft = {
-  artistName: "Shroomie",
-  artistAvatar: artistAvatar,
-  name: "Magic Mushrooms",
-  content: nftContent,
-  price: "0.0031937",
-  fiatPrice: "4.95",
-  isAvailable: true,
-};
+import { useLocation } from "react-router-dom";
 
 function Details() {
+  const location = useLocation();
+  const nft = location.state.item;
   return (
     <>
       <section className="flex flex-col p-2 gap-4 md:flex-row lg:justify-between m-auto max-w-[1280px]">
-        <NFTCard content={nft.content} isAvailable={nft.isAvailable} />
+        <NFTCard image={nft.image} isAvailable={nft.isAvailable} />
         <div className="md:w-3/6">
           <MakeOffer nft={nft} />
           <AvailableListings />
