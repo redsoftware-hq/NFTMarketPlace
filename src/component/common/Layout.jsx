6d35403083ab96022df93d63c674daf1ccf99067
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-17 15:48:04 +0530

    fix: import updated #2

--- src/component/common/Layout.jsx
+++ src/component/common/Layout.jsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../component/Navbar/Navbar";
-import Footer from "../component/Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 function Layout() {
-	return (
-		<>
-			<Navbar />
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
