a27c81bf79234bc66965254dd2bf1a35b19780da
Your Name <mukulvishwakarma099@gmail.com>
2023-02-20 13:13:13 +0530

    add some changes in discover section

--- src/apis/cryptoApi.js
+++ src/apis/cryptoApi.js
@@ -2,126 +2,126 @@ import { ethers } from "ethers";
 import { JSONRPCClient } from "json-rpc-2.0";
 
 export async function mintWalletNew({
-	walletAddress,
-	blockchain,
-	signedString,
-	signedKeyLinking,
+  walletAddress,
+  blockchain,
+  signedString,
+  signedKeyLinking,
 }) {
-	const params = [
-		{
-			customer: {
-				customerId: "321385513",
-				metaverse: {
-					metaverseName: "IMVU",
-					metaverseId: "IMVU_Production",
-				},
-			},
-		},
-		{
-			wallet: {
-				walletAddress: walletAddress,
-				blockchain: {
-					name: blockchain,
-				},
-			},
-		},
-		{
-			text: {
-				name: "SignedString",
-				data: signedString,
-			},
-		},
-		{
-			text: {
-				name: "SignedKeyLinking",
-				data: signedKeyLinking,
-			},
-		},
-		{
-			text: {
-				name: "TypeOfWallet",
-				data: "L2WalletOnImmutableX",
-			},
-		},
-	];
+  const params = [
+    {
+      customer: {
+        customerId: "321385513",
+        metaverse: {
+          metaverseName: "IMVU",
+          metaverseId: "IMVU_Production",
+        },
+      },
+    },
+    {
+      wallet: {
+        walletAddress: walletAddress,
+        blockchain: {
+          name: blockchain,
+        },
+      },
+    },
+    {
+      text: {
+        name: "SignedString",
+        data: signedString,
+      },
+    },
+    {
+      text: {
+        name: "SignedKeyLinking",
+        data: signedKeyLinking,
+      },
+    },
+    {
+      text: {
+        name: "TypeOfWallet",
+        data: "L2WalletOnImmutableX",
+      },
+    },
+  ];
 
-	try {
-		await metaJuiceClient("mint_wallet", params);
-		// Return wallet with signed strings
-		return { walletAddress, blockchain, signedString, signedKeyLinking };
-	} catch (e) {
-		console.error(e);
-		throw e;
-	}
+  try {
+    await metaJuiceClient("mint_wallet", params);
+    // Return wallet with signed strings
+    return { walletAddress, blockchain, signedString, signedKeyLinking };
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 function metaJuiceClient(method, params) {
-	const url = "https://dev-cryptoapi.metajuice.link/v2/";
-	const client = new JSONRPCClient((jsonRPCRequest) =>
-		fetch(url, {
-			method: "POST",
-			headers: {
-				"content-type": "application/json",
-				"x-api-key": "216ggx2qQJ1w4c02cKtfF5bE0f4bvBsB47DpGT4o",
-			},
-			body: JSON.stringify(jsonRPCRequest),
-		}).then((response) => {
-			if (response.status === 200) {
-				return response.json().then((jsonRPCResponse) => {
-					if ("error" in jsonRPCResponse) {
-						openNotificationWithIcon(
-							"error",
-							JSON.parse(jsonRPCResponse.error.data).message
-						);
-						return Promise.reject(
-							new Error(extractFullReturnError(jsonRPCResponse.error))
-						);
-					}
-					return client.receive(jsonRPCResponse);
-				});
-			} else if (!jsonRPCRequest.id) {
-				return Promise.reject(new Error(response.statusText));
-			}
-		})
-	);
-	return client.request(method, params);
+  const url = "https://dev-cryptoapi.metajuice.link/v2/";
+  const client = new JSONRPCClient((jsonRPCRequest) =>
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "x-api-key": "216ggx2qQJ1w4c02cKtfF5bE0f4bvBsB47DpGT4o",
+      },
+      body: JSON.stringify(jsonRPCRequest),
+    }).then((response) => {
+      if (response.status === 200) {
+        return response.json().then((jsonRPCResponse) => {
+          if ("error" in jsonRPCResponse) {
+            openNotificationWithIcon(
+              "error",
+              JSON.parse(jsonRPCResponse.error.data).message
+            );
+            return Promise.reject(
+              new Error(extractFullReturnError(jsonRPCResponse.error))
+            );
+          }
+          return client.receive(jsonRPCResponse);
+        });
+      } else if (!jsonRPCRequest.id) {
+        return Promise.reject(new Error(response.statusText));
+      }
+    })
+  );
+  return client.request(method, params);
 }
 
 export const updateBalanceAsync = async (wallet) => {
-	try {
-		const response = await fetchFungibleForWallet(wallet);
-		const sanitizedResults = response.map(
-			({ fungibletoken: { tokenType, amount } }) => ({
-				tokenType,
-				amount,
-				lastUpdate: new Date().toJSON(),
-			})
-		);
+  try {
+    const response = await fetchFungibleForWallet(wallet);
+    const sanitizedResults = response.map(
+      ({ fungibletoken: { tokenType, amount } }) => ({
+        tokenType,
+        amount,
+        lastUpdate: new Date().toJSON(),
+      })
+    );
 
-		// The value we return becomes the `fulfilled` action payload
-		return sanitizedResults;
-	} catch (error) {
-		console.log(error);
-	}
+    // The value we return becomes the `fulfilled` action payload
+    return sanitizedResults;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export async function fetchFungibleForWallet({ walletAddress, blockchain }) {
-	const params = [
-		{
-			wallet: {
-				walletAddress: walletAddress,
-				blockchain: {
-					name: blockchain,
-				},
-			},
-		},
-		{
-			text: {
-				name: "TokenType",
-				data: "ETH",
-			},
-		},
-	];
+  const params = [
+    {
+      wallet: {
+        walletAddress: walletAddress,
+        blockchain: {
+          name: blockchain,
+        },
+      },
+    },
+    {
+      text: {
+        name: "TokenType",
+        data: "ETH",
+      },
+    },
+  ];
 
-	return await metaJuiceClient("fetch_fungibleForWallet", params);
+  return await metaJuiceClient("fetch_fungibleForWallet", params);
 }
--- src/component/Home/Cards/DiscoverCard.jsx
+++ src/component/Home/Cards/DiscoverCard.jsx
@@ -1,37 +1,37 @@
 const DiscoverCard = ({ item }) => {
-	return (
-		<>
-			<div className="w-full hover:scale-105 duration-300 cursor-pointer">
-				<div>
-					<img className="w-full" src={item.image} alt="" />
-				</div>
-				<div className="bg-[#3b3b3b] rounded-b-2xl py-5 px-7 space-y-5">
-					<div className="text-white">
-						<p className="text-xl font-bold">{item.imgTitle}</p>
-						<p className="pt-2 flex items-center gap-3">
-							<span>
-								<img src={item.avatar} alt="" />
-							</span>
-							<span className="font-space-mono">{item.avatarName}</span>
-						</p>
-					</div>
+  return (
+    <>
+      <div className="w-full hover:scale-105 duration-300 cursor-pointer">
+        <div className="">
+          <img className="w-full rounded-t-2xl" src={item.image} alt="" />
+        </div>
+        <div className="bg-[#3b3b3b] rounded-b-2xl py-5 px-7 space-y-5">
+          <div className="text-white">
+            <p className="text-xl font-bold">{item.imgTitle}</p>
+            <p className="pt-2 flex items-center gap-3">
+              <span>
+                <img src={item.avatar} alt="" />
+              </span>
+              <span className="font-space-mono">{item.avatarName}</span>
+            </p>
+          </div>
 
-					<div className="font-space-mono flex justify-between">
-						<div className="price-div ">
-							<span className="text-[#858584] text-xs">Price</span>
-							<p className="text-white">{item.price}</p>
-						</div>
-						<div className="bid-div">
-							<span className="capitalize text-[#858584] text-xs">
-								highest bid
-							</span>
-							<p className="text-white">{item.highestBid}</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+          <div className="font-space-mono flex justify-between">
+            <div className="price-div ">
+              <span className="text-[#858584] text-xs">Price</span>
+              <p className="text-white">{item.price}</p>
+            </div>
+            <div className="bid-div">
+              <span className="capitalize text-[#858584] text-xs">
+                highest bid
+              </span>
+              <p className="text-white">{item.highestBid}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DiscoverCard;
