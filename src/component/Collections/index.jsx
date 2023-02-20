b48117c74d4f79c1273153e3201ab589fd8c87ef
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-20 18:26:11 +0530

    Add: route and index page defined

---
+++ src/component/Collections/index.jsx
@@ -0,0 +1,11 @@
+import MoreFromArtist from "./MoreFromArtist/MoreFromArtist";
+
+function Collections() {
+  return (
+    <section>
+      <MoreFromArtist />
+    </section>
+  );
+}
+
+export default Collections;
