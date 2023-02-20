7d72cf28ae1574d8aa4ce856e55a3a9262926363
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-20 18:25:47 +0530

    Add: can also add classnames

--- src/component/common/Buttons/SecondaryButton.jsx
+++ src/component/common/Buttons/SecondaryButton.jsx
@@ -1,6 +1,8 @@
-function SecondaryButton({ btnName }) {
+function SecondaryButton({ btnName, className }) {
   return (
-    <button className="py-3 px-10 bg-[#2b2b2b] border-2 border-[#F15623] hover:text-[#F15623] ease-in-out duration-300 rounded-2xl capitalize text-white text-base font-medium">
+    <button
+      className={`${className} py-3 px-10 bg-[#2b2b2b] border-2 border-[#F15623] hover:text-[#F15623] ease-in-out duration-300 rounded-2xl capitalize text-white text-base font-medium`}
+    >
       {btnName}
     </button>
   );
