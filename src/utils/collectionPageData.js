664d06777eefcd1b55ae5f15a9e9fd62c35879a9
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-21 15:04:00 +0530

    Add: artist and collection data models.

--- src/utils/collectionPageData.js
+++ src/utils/collectionPageData.js
@@ -5,6 +5,18 @@ import w2 from "../assets/collectionsPage/artistWorks/w2.png";
 import w3 from "../assets/collectionsPage/artistWorks/w3.png";
 import w4 from "../assets/collectionsPage/artistWorks/w4.png";
 export const HEADER = bg;
+export const ARTIST = {
+  name: "Orbitian",
+  avatar: avatar,
+};
+export const COLLECTION = {
+  name: "The Orbitians",
+  minted: "Sep 30, 2022",
+  description: `The Orbitians is a collection of 10,000 unique NFTs on the Ethereum blockchain,
+  There are all sorts of beings in the NFT Universe. The most advanced and friendly of the bunch are Orbitians.
+  They live in a metal space machines, high up in the sky and only have one foot on Earth. These Orbitians are a peaceful race, but they have been at war with a group of invaders for many generations. The invaders are called Upside-Downs, because of their inverted bodies that live on the ground, yet do not know any other way to be. Upside-Downs believe that they will be able to win this war if they could only get an eye into Orbitian territory, so they've taken to make human beings their target.  
+  `,
+};
 export const artistWorks = [
   {
     id: 1,
