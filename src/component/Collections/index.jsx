f2e46ccc9eb620774403131117deffb11b714b20
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-21 15:05:25 +0530

    Add: index layout defined

--- src/component/Collections/index.jsx
+++ src/component/Collections/index.jsx
@@ -1,8 +1,18 @@
+import Header from "./Header/Header";
+import Hero from "./Hero/Hero";
 import MoreFromArtist from "./MoreFromArtist/MoreFromArtist";
+import Timer from "./Timer/Timer";
 
 function Collections() {
   return (
     <section>
+      <Header />
+      <div className="flex md:p-10 lg:px-20 lg:py-10">
+        <Hero />
+        <div className="hidden mb-auto ml-auto md:flow-root">
+          <Timer />
+        </div>
+      </div>
       <MoreFromArtist />
     </section>
   );
