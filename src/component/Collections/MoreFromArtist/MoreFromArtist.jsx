e90f9c3fc4c9a5098401a6385fc538b379139eb8
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-21 15:04:30 +0530

    Style: fix padding and width

--- src/component/Collections/MoreFromArtist/MoreFromArtist.jsx
+++ src/component/Collections/MoreFromArtist/MoreFromArtist.jsx
@@ -14,17 +14,17 @@ export default function MoreFromArtist() {
             btnName="Go to artist page"
           />
         </div>
-        <div className="mt-10 creators-card grid content-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 md:gap-8">
+        <div className="mt-10 creators-card grid content-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-8">
           {artistWorks &&
             artistWorks.map((item, index) => {
               return <DiscoverCard key={index} item={item} />;
             })}
         </div>
-
-        <div className="py-4">
-          <button className="w-full mt-8 py-5 px-10 flex items-center justify-center md:hidden lg:hidden bg-[#2b2b2b] border-2 border-[#F15623] hover:text-[#F15623] ease-in-out duration-300 rounded-2xl capitalize text-white text-sm font-semibold gap-2 lg:mr-8">
-            view rankings
-          </button>
+        <div className="px-1 py-8">
+          <SecondaryButton
+            className="w-full md:hidden lg:hidden"
+            btnName="Go to artist page"
+          />
         </div>
       </div>
     </section>
