9994822925f8c0d30590f72321de9df0f89ca4b9
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-23 17:38:47 +0530

    Add: route added for create-collection

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,27 @@ import Collections from "./component/Collections";
 import Marketplace from "./component/Marketplace";
 import { createRoutesFromElements } from "react-router";
 import { RouterProvider, createBrowserRouter, Route } from "react-router-dom";
+import CreateCollection from "./component/CreateCollection";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<Layout />}>
-			<Route index element={<Home />} />
-			<Route path="marketplace" element={<Marketplace />} />
-			<Route path="rankings" element={<Rankings />} />
-			<Route path="signup" element={<SignUp />} />
-			<Route path="details/:id" element={<Details />} />
-			<Route path="collections" element={<Collections />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="marketplace" element={<Marketplace />} />
+      <Route path="rankings" element={<Rankings />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="details/:id" element={<Details />} />
+      <Route path="collections" element={<Collections />} />
+      <Route
+        path="collections/create-collection"
+        element={<CreateCollection />}
+      />
+    </Route>
+  )
 );
+
 function App() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
