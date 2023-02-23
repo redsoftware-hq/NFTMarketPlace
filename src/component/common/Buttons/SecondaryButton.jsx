9c6cf0e2ffc0e5ec1d5cb16226a387089b69b1dc
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-23 17:39:25 +0530

    fix: handleClick prop missing

--- src/component/common/Buttons/SecondaryButton.jsx
+++ src/component/common/Buttons/SecondaryButton.jsx
@@ -1,6 +1,8 @@
-function SecondaryButton({ btnName, className }) {
+function SecondaryButton({ btnName, className, handleClick, type }) {
   return (
     <button
+      type={type}
+      onClick={handleClick}
       className={`${className} py-3 px-10 bg-[#2b2b2b] border-2 border-[#F15623] hover:text-[#F15623] ease-in-out duration-300 rounded-2xl capitalize text-white text-base font-medium`}
     >
       {btnName}
