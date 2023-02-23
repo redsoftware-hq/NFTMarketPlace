691b32971a1b035ad0d4a7058ebcb245499054f1
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-23 17:42:42 +0530

    Add: page structure for create-collection.

---
+++ src/component/CreateCollection/index.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header/Header";
+import CollectionForm from "./CollectionForm/CollectionForm";
+function CreateCollection() {
+  return (
+    <section className="px-1 grid justify-center gap-2 md:m-auto md:max-w-[768px]">
+      <Header />
+      <CollectionForm />
+    </section>
+  );
+}
+
+export default CreateCollection;
