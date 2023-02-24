b33625d91c26533a61ad60aee546b273c6751d33
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-24 12:13:38 +0530

    feat: can utilize options for additional checks.

--- src/component/common/Form/Input.jsx
+++ src/component/common/Form/Input.jsx
@@ -1,11 +1,11 @@
-const Input = ({ name, type, placeholder, register, id }) => {
+const Input = ({ name, type, placeholder, register, id, options = {} }) => {
   return (
     <input
       id={id}
       type={type}
       className="p-3 rounded-2xl w-full caret-[#f15623] focus:accent-[#f15623]"
       placeholder={placeholder}
-      {...register(name)}
+      {...register(name, options)}
     />
   );
 };
--- src/component/common/Form/TextArea.jsx
+++ src/component/common/Form/TextArea.jsx
@@ -1,10 +1,10 @@
-const TextArea = ({ name, register, rows, id }) => {
+const TextArea = ({ name, register, rows, id, options = {} }) => {
   return (
     <textarea
       id={id}
       className="p-3 rounded-2xl w-full caret-[#f15623] focus:accent-[#f15623]"
       rows={rows}
-      {...register(name)}
+      {...register(name, options)}
     />
   );
 };
