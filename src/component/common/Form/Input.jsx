f4420a81a06152abeffda1c2c174ee025fd593fb
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-24 12:49:16 +0530

    style: hide on demand

--- src/component/common/Form/Input.jsx
+++ src/component/common/Form/Input.jsx
@@ -1,9 +1,12 @@
-const Input = ({ name, type, placeholder, register, id, options = {} }) => {
+import React from 'react';
+
+const Input = ({ name, type, placeholder, register, id, options = {}, show = true }) => {
+  const opacity = show ? '' : 'opacity-0';
   return (
     <input
       id={id}
       type={type}
-      className="p-3 rounded-2xl w-full caret-[#f15623] focus:accent-[#f15623]"
+      className={`${opacity} p-3 rounded-2xl w-full caret-[#f15623] focus:accent-[#f15623]`}
       placeholder={placeholder}
       {...register(name, options)}
     />
