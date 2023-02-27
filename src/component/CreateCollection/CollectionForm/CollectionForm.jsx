f72e5035d461230616cd0fabe43723a26abe9b31
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-27 13:13:11 +0530

    refactored image upload into component

--- src/component/CreateCollection/CollectionForm/CollectionForm.jsx
+++ src/component/CreateCollection/CollectionForm/CollectionForm.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import { useForm } from 'react-hook-form';
 import PrimaryButton from '../../common/Buttons/PrimaryButton';
 import Input from '../../common/Form/Input';
 import TextArea from '../../common/Form/TextArea';
-import { BiImageAdd } from 'react-icons/bi';
+import UploadImage from '../../common/Form/UploadImage';
 
 const emailPattern =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -29,30 +28,7 @@ function CollectionForm() {
     <form
       className="px-1 flex flex-col gap-3 font-work-sans"
       onSubmit={handleSubmit((data) => console.log(data))}>
-      <div className="upload inline-grid p-1 gap-3">
-        <label className="px-2 text-white" htmlFor={labels.upload}>
-          Upload Image *
-        </label>
-        <span className="px-2 text-[#858584]">
-          This image will be used for featuring your collection on the homepage, category pages, or
-          other promotional areas of Metajuice. 600 x 400 recommended.
-        </span>
-        <div className="m-auto sm:m-0 flex flex-col justify-center items-center w-64 h-64 sm:w-72 sm:h-72 md:w-96 md:h-96 rounded-2xl bg-[#3b3b3b] cursor-pointer">
-          <label
-            className="w-full h-full cursor-pointer flex flex-col justify-center items-center"
-            htmlFor={labels.upload}>
-            <BiImageAdd className="text-white w-2/4 h-2/4" />
-            <Input
-              id={labels.upload}
-              name={labels.upload}
-              type="file"
-              register={register}
-              show={false}
-            />
-          </label>
-        </div>
-      </div>
-
+      <UploadImage register={register} />
       <div className="collection-name inline-grid p-1 gap-1">
         <label className="px-2 text-white" htmlFor={labels.name}>
           Name *
