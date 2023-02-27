c19dc7980ecedb703b564eb39aefda5027ec37cb
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-27 11:39:38 +0530

    renamed  header to form header

--- src/component/CreateCollection/index.jsx
+++ src/component/CreateCollection/index.jsx
@@ -1,9 +1,11 @@
-import Header from './Header/Header';
+import React from 'react';
+import FormHeader from '../common/Form/FormHeader';
 import CollectionForm from './CollectionForm/CollectionForm';
+
 function CreateCollection() {
   return (
     <section className="px-1 grid justify-center gap-2 md:m-auto md:max-w-[768px]">
-      <Header />
+      <FormHeader title="Create a collection" />
       <CollectionForm />
     </section>
   );
--- src/component/common/Form/FormHeader.jsx
+++ src/component/common/Form/FormHeader.jsx
@@ -1,7 +1,8 @@
-export default function Header() {
+import React from 'react';
+export default function FormHeader({ title }) {
   return (
     <div className="inline-grid gap-1 p-1">
-      <h1 className="px-2 text-white text-4xl font-work-sans font-semibold">Create a Collection</h1>
+      <h1 className="px-2 text-white text-4xl font-work-sans font-semibold">{title}</h1>
       <p className="px-2 text-white font-space-mono text-xs">
         <span className="px-1 text-red-500">*</span>
         Required Fields
