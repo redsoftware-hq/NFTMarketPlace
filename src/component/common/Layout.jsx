161bb7b40ef3a419293751868553abb6551c57cf
Vibek Dutta <vikiisbck@gmail.com>
2023-03-02 16:27:33 +0530

    Add: Added listNftForSale with nonce signed

--- src/component/common/Layout.jsx
+++ src/component/common/Layout.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import Footer from './Footer';
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 import Navbar from './Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-import {listNftForSaleNonce} from '../../apis/cryptoApi';
+import { listNftForSaleNonce, listNftForSale } from '../../apis/cryptoApi';
 
 function Layout() {
-
   // React.useEffect(() => {
+  //   let listNftForSaleResponse;
+  //   let listNftForSaleNonceResponse;
+
   //   const apiCall = async () => {
   //     const provider = new ethers.providers.Web3Provider(window?.ethereum, 'goerli');
   //     const requestAccounts = await provider.send('eth_requestAccounts', []);
   //     const network = await provider.getNetwork();
   //     const account = requestAccounts[0];
-  //     const signer = provider.getSigner();
-  //     const signedString = await signer.signMessage(
-  //       'Only sign this request if you’ve initiated an action with Immutable X.'
-  //     );
-  //     await listNftForSaleNonce({
+  //     listNftForSaleNonceResponse = await listNftForSaleNonce({
   //       walletAddress: account,
   //       blockchain: 'Ethereum_' + network.name,
   //       contractAddress: '0xa4de988522a68a4f07aba97043e5266e8465b622',
   //       tokenId: '78216660'
   //     });
+
+  //     const signer = provider.getSigner();
+  //     const signedString = await signer.signMessage(
+  //       'Only sign this request if you’ve initiated an action with Immutable X.'
+  //     );
+  //     listNftForSaleResponse = await listNftForSale({
+  //       walletAddress: account,
+  //       blockchain: 'Ethereum_' + network.name,
+  //       contractAddress: '0xa4de988522a68a4f07aba97043e5266e8465b622',
+  //       tokenId: '78216660',
+  //       nonce: listNftForSaleNonceResponse.nonce,
+  //       signedString
+  //     });
   //   };
+
   //   apiCall();
   // }, []);
 
