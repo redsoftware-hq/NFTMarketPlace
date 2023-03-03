63139582ce2d699910de652e42e65ee280759e50
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-03-03 10:52:59 +0530

    Style: ui for connect wallet updated.

--- src/component/common/Navbar/Navbar.jsx
+++ src/component/common/Navbar/Navbar.jsx
@@ -135,44 +135,43 @@ const Navbar = () => {
               Rankings
             </NavLink>
             <div className="flex justify-between items-center gap-2">
-              <div>
-                <button className="hover:text-[#F15623] duration-300" onClick={handleConnect}>
+              <div className="bg-[#F15623] py-4 px-7 rounded-2xl flex items-center gap-2 border-2 border-[transparent] hover:border-[#F15623] hover:bg-transparent duration-300">
+                <button className="" onClick={handleConnect}>
                   {walletData.walletAddress ? 'Wallet Connected' : 'Connect Wallet'}
                 </button>
-              </div>
-
-              {error ? (
-                <Toast message={"Couldn't connect wallet"} type={'error'} />
-              ) : (
-                <>
-                  {walletData.walletAddress && walletData.network && (
-                    <div className="relative group cursor-pointer">
-                      <AiOutlineInfoCircle />
-                      <div className="absolute z-10 py-3 px-5 hidden w-[200px] text-sm text-white bg-[#3b3b3b] rounded-lg shadow-md top-8 right-0 group-hover:block space-y-3">
-                        <div className="flex flex-col mt-1">
-                          <span className="font-normal">Address:</span>
-                          <span className="font-light">{`${walletData.walletAddress.substring(
-                            0,
-                            9
-                          )}....${walletData.walletAddress.substring(
-                            walletData.walletAddress.length - 9
-                          )}`}</span>
-                        </div>
-                        <div className="flex flex-col">
-                          <span className="font-normal">Connected To:</span>
-                          <span className="font-light">{walletData.network.name}</span>
-                        </div>
-                        <div className="flex flex-col">
-                          <span className="font-normal">Wallet Balance:</span>
-                          <span className="font-light">
-                            {`${Number(walletData.balance).toFixed(2)} eth`}
-                          </span>
+                {error ? (
+                  <Toast message={"Couldn't connect wallet"} type={'error'} />
+                ) : (
+                  <>
+                    {walletData.walletAddress && walletData.network && (
+                      <div className="relative group cursor-pointer">
+                        <AiOutlineInfoCircle />
+                        <div className="absolute z-10 py-3 px-5 hidden w-[200px] text-sm text-white bg-[#3b3b3b] rounded-lg shadow-md top-8 right-0 group-hover:block space-y-3">
+                          <div className="flex flex-col mt-1">
+                            <span className="font-normal">Address:</span>
+                            <span className="font-light">{`${walletData.walletAddress.substring(
+                              0,
+                              9
+                            )}....${walletData.walletAddress.substring(
+                              walletData.walletAddress.length - 9
+                            )}`}</span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="font-normal">Connected To:</span>
+                            <span className="font-light">{walletData.network.name}</span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="font-normal">Wallet Balance:</span>
+                            <span className="font-light">
+                              {`${Number(walletData.balance).toFixed(2)} eth`}
+                            </span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  )}
-                </>
-              )}
+                    )}
+                  </>
+                )}
+              </div>
             </div>
             <button
               onClick={() => navigate('/create-account')}
