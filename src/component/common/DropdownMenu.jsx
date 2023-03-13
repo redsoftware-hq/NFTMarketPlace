22f9b5443e6a0ac61e8a90f55d04abedb1d2d649
Your Name <mukulvishwakarma099@gmail.com>
2023-03-13 12:45:54 +0530

    resolved PR comments

--- src/component/common/DropdownMenu.jsx
+++ src/component/common/DropdownMenu.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ethers } from 'ethers';
-// import { listNftForSaleNonce, listNftForSale } from '../../apis/cryptoApi';
 
 const DropdownMenu = () => {
   // let listNftForSaleResponse;
