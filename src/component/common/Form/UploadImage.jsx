a2421ba0f5c0d21c63cbbd8014bef94dafb776e8
Your Name <mukulvishwakarma099@gmail.com>
2023-03-21 15:39:20 +0530

    Feature: preview image of selected file

--- src/component/common/Form/UploadImage.jsx
+++ src/component/common/Form/UploadImage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../common/Form/Input';
 import { BiImageAdd } from 'react-icons/bi';
 
 export default function UploadImage({ register, options = {}, upload = 'upload' }) {
+  const [file, setFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState('');
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    setFile(file);
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+  }
   return (
     <div className="upload inline-grid p-1 gap-3">
       <label className="px-2 text-white" htmlFor={upload}>
@@ -13,13 +26,39 @@ export default function UploadImage({ register, options = {}, upload = 'upload' 
         This image will be used for featuring your collection on the homepage, category pages, or
         other promotional areas of Metajuice. 600 x 400 recommended.
       </span>
-      <div className="m-auto sm:m-0 flex flex-col justify-center items-center w-64 h-64 sm:w-72 sm:h-72 md:w-96 md:h-96 rounded-2xl bg-[#3b3b3b] cursor-pointer">
+      <div className="m-auto sm:m-0 flex flex-col justify-center items-center w-[250px] h-[250px] md:w-[350px] md:h-[350px] rounded-2xl bg-[#3b3b3b] cursor-pointer">
         <label
           className="w-full h-full cursor-pointer flex flex-col justify-center items-center"
-          htmlFor={upload}
-        >
-          <BiImageAdd className="text-white w-2/4 h-2/4" />
-          <Input name={upload} type="file" register={register} show={false} options={options} />
+          htmlFor={upload}>
+          {file ? (
+            <div className="imagePreview w-[250px] h-[250px] md:w-[350px] md:h-[350px]">
+              <img
+                className="w-full h-full object-cover rounded-2xl"
+                src={imagePreview}
+                alt="Preview"
+              />
+              <Input
+                name={upload}
+                type="file"
+                register={register}
+                show={false}
+                options={options}
+                handleFileChange={handleFileChange}
+              />
+            </div>
+          ) : (
+            <div className="imagePreview w-[250px] h-[250px] md:w-[350px] md:h-[350px]">
+              <BiImageAdd className="text-white w-full h-full" />
+              <Input
+                name={upload}
+                type="file"
+                register={register}
+                show={false}
+                options={options}
+                handleFileChange={handleFileChange}
+              />
+            </div>
+          )}
         </label>
       </div>
     </div>
